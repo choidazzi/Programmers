102e0744d7b4ae322b2d921e896aa44af81e56b4
ChoiDazzi <127722626+ChoiDazzi@users.noreply.github.com>
2023-11-01 18:03:54 +0900

    [level unrated] Title: A 강조하기, Time: 0.06 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        String[] strs = myString.split("");
-        for(int i = 0; i < strs.length; i++) {
-            if(strs[i].equals("a") || strs[i].equals("A")) {
-                answer += strs[i].toUpperCase();
-            } else {
-                answer += strs[i].toLowerCase();
-            }
-        }
+        myString = myString.toLowerCase();
         
-        return answer;
+        return myString.replaceAll("a", "A");
     }
 }
